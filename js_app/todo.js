c1c18b414f6ae5cbcf25d2b7ec1d9758580a7c46
kih1024 <rladlsgh654@naver.com>
2019-11-11 17:53:54 +0900

    Add delete function

--- js_app/todo.js
+++ js_app/todo.js
@@ -4,15 +4,41 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS =  'toDos';
 
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));//localstorage는 데이터를 string 형태로밖에 저장 못함. JSON.stringify는 오브젝트를 string 형태로 바꿔줌
+}
+
+function deleteToDo(event){
+    //console.dir로 parentNode 같은 것을 찾는다.
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });//각가의 object 들을 돌려서 참인것들의 array를 리턴, 삭제되지 않은 toDo 들을 리턴한다.
+    toDos = cleanToDos;
+    saveToDos();
+}
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "❌"
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerHTML = "❌"
+    delBtn.addEventListener("click",deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -22,9 +48,12 @@ function handleSubmit(event){
     toDoInput.value = "";
 }
 function loadToDos(){ 
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadToDos = localStorage.getItem(TODOS_LS);
+    if(loadToDos !== null){
+        const parseToDos = JSON.parse(loadToDos);
+        parseToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     }
 }
 
